45ad710eb900298dcb0729bdfb7c2a5805630d4b
shperry <shperry@crimson.ua.edu>
2022-11-07 14:06:13 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+const { contextIsolated } = require('process')
+
+const dgram = require("dgram");
+// const commandDelays = require("./commandDelays");
+//goes from the backend(robot information) to the frontend (User Interface)
+const app1 = require("express")();
+const server = require("http").createServer(app1);
+const io = require("socket.io")(server);
+
+const Port = 8889;
+const StatePort = 8890;
+const Host = "192.168.10.1";
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  // and load the index.html of the app.
+  console.log("Connecting to drone....")
+
+  const drone = dgram.createSocket("udp4");
+  drone.bind(Port);
+  const droneState = dgram.createSocket("udp4");
+  droneState.bind(StatePort);
+  io.on("connect", socket => {
+    setInterval(moveDrone, frequency);
+    socket.on("command", command => {
+      console.log("command sent from browser: ", command);
+      drone.send(command, 0, command.length, Port, Host, handleError);
+    });
+    socket.emit("status", "CONNECTED");
+  });
+
+  mainWindow.loadFile('src/index.html')
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ src/index.js
@@ -0,0 +1,109 @@
+const fs = require('fs')
+const dgram = require("dgram");
+// const commandDelays = require("./commandDelays");
+//goes from the backend(robot information) to the frontend (User Interface)
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const Port = 8889;
+const StatePort = 8890;
+const Host = "192.168.10.1";
+
+/**
+ * Set up buttons for application
+ * 
+ * ADD MORE BUTTONS FOR FUNCTIONALITY
+ */
+const upButton = document.getElementById('up-button')
+const leftButton = document.getElementById('left-button')
+const rightButton = document.getElementById('right-button')
+const downButton = document.getElementById('down-button')
+
+upButton.addEventListener('click', function(event){
+    moveDrone("up")
+})
+
+leftButton.addEventListener('click', function(event){
+    moveDrone("left")
+})
+
+rightButton.addEventListener('click', function(event){
+    moveDrone("right")
+})
+
+downButton.addEventListener('click', function(event){
+    moveDrone("down")
+})
+
+//backend: goes from robot to backend
+// const drone = dgram.createSocket("udp4");
+// drone.bind(Port);
+// const droneState = dgram.createSocket("udp4");
+// droneState.bind(StatePort);
+
+let drone_takeoff = false;
+
+const handleError = err => {
+    if (err) {
+      return "This is the error: ", err;
+    }
+};
+
+//When the server comes on:
+// io.on("connect", socket => {
+//     setInterval(moveDrone, frequency);
+//     socket.on("command", command => {
+//       console.log("command sent from browser: ", command);
+//       drone.send(command, 0, command.length, Port, Host, handleError);
+//     });
+//     socket.emit("status", "CONNECTED");
+// });
+
+const frequency = 3500;
+let amplitude = 30;
+
+function crtCMD(direction, distance) {
+    return direction + " " + distance;
+}
+  
+function command(direction, distance) {
+    let cmd = crtCMD(direction, distance);
+    drone.send(cmd, 0, cmd.length, Port, Host, handleError);
+}
+
+const start = () => {
+    console.log("drone is taking off");
+    drone_takeoff = true;
+    drone.send("takeoff", 0, 8, Port, Host, handleError);
+};
+
+//MOVE THE DRONE 
+function moveDrone(direction){
+    console.log(direction)
+    fs.appendFileSync('./data.txt',direction +  "\n")
+
+    if (direction == "up") {
+        command("up", amplitude);
+    } else if (direction == "down"){
+        command("down", amplitude);
+    } else if (direction == "right"){
+        command("right", amplitude);
+    } else if (direction == "left"){
+        command("left", amplitude);
+    }
+    
+    if (drone_takeoff == false) {
+        start();
+    }
+};
+
+server.listen("6767", () => {
+    console.log("up and running...");
+  });
+  
+drone.on("message", message => {
+    console.log("${message}");
+});
+  
+drone.send("command", 0, 8, Port, Host, handleError);
